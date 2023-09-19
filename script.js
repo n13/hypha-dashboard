92509921aff4ff195ed9cf3e287a84963405f57e
NIK <nheger@gmail.com>
2023-09-19 22:36:27 +0800

    update - show tiers

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { JsonRpc } from 'eosjs';
+const { JsonRpc } = require('eosjs'); // Require JsonRpc from eosjs
 
 const accountInfo = document.getElementById('account-info');
 
@@ -6,12 +6,13 @@ const accountInfo = document.getElementById('account-info');
 const urlParams = new URLSearchParams(window.location.search);
 const accountName = urlParams.get('account');
 
-async function fetchLockedUpTokens() {
-    try {
-        // Initialize EOSJS with the Telos EOSIO endpoint URL
-        const rpc = new JsonRpc('https://mainnet.telos.net');
+// Replace 'https://mainnet.telos.net' with your actual EOSIO endpoint URL
+const rpc = new JsonRpc('https://mainnet.telos.net');
 
-        // Replace 'vestng.hypha' with your contract account name and 'locks' with your table name
+// Function to fetch data from the EOSIO contract
+async function fetchData() {
+    try {
+        // Make a request to the contract's table
         const response = await rpc.get_table_rows({
             json: true,
             code: 'vestng.hypha',
@@ -20,33 +21,81 @@ async function fetchLockedUpTokens() {
             index_position: 2,
             key_type: 'name',
             lower_bound: accountName,
-            upper_bound: accountName,
+            upper_bound: accountName,            
         });
 
-        // res: {
-        //     "rows": [
-        //       {
-        //         "lock_id": 0,
-        //         "owner": "illumination",
-        //         "tier_id": "launch",
-        //         "amount": "2.00 HYPHA",
-        //         "claimed_amount": "0.00 HYPHA",
-        //         "note": "bdc51c7d17c6a2afcba1e4ff8551c83d10ae340499fe4a362d5133d29f41b9fa"
-        //       },
-        // ...
-
-
-        console.log("locks for " + accountName);
-        console.log("res: " + JSON.stringify(response, null, 2));
-
-        // Extract and display the locked-up token information
-        const lockedUpTokens = response.rows[0]; // Adjust this based on your contract structure
-        accountInfo.innerHTML = `<p>Account: ${accountName}</p>`;
-        accountInfo.innerHTML += `<p>Locked Up Tokens: ${lockedUpTokens}</p>`;
+        return response.rows;
     } catch (error) {
-        console.error('Error fetching data:', error);
+        throw error;
     }
 }
 
-// Call the function to fetch and display locked-up tokens
-fetchLockedUpTokens();
+// Function to calculate the list of tiers and total amounts
+function calculateTiersAndTotals(data) {
+    const tiersAndTotals = {};
+
+    // Group the results by tier and calculate total amounts
+    data.forEach(row => {
+        const tierId = row.tier_id;
+        if (!tiersAndTotals[tierId]) {
+            tiersAndTotals[tierId] = {
+                totalAmount: 0,
+                rows: [],
+            };
+        }
+
+        const amountParts = row.amount.split(' ');
+        if (amountParts.length === 2 && amountParts[1] === 'HYPHA') {
+            tiersAndTotals[tierId].totalAmount += parseFloat(amountParts[0]);
+        }
+
+        tiersAndTotals[tierId].rows.push(row);
+    });
+
+    return tiersAndTotals;
+}
+
+// Function to render the list of tiers and total amounts
+function renderTiersAndTotals(tiersAndTotals) {
+    // Get a reference to the HTML element where you want to display the results
+    const resultsContainer = document.getElementById('results-container');
+    const accountInfoDiv = document.getElementById('account-info');
+    
+    // Display the account name in the account-info div
+    accountInfoDiv.textContent = `Account: ${accountName || 'N/A'}`;
+
+    // Iterate through the tiers and total amounts
+    for (const tierId in tiersAndTotals) {
+        const tierData = tiersAndTotals[tierId];
+
+        // Create a section for each tier
+        const tierSection = document.createElement('div');
+        tierSection.className = 'tier-section';
+
+        // Display the tier information
+        tierSection.innerHTML = `
+            <h2>Tier: ${tierId}</h2>
+            <p>Total Amount: ${tierData.totalAmount} HYPHA</p>
+            <ul>
+                ${tierData.rows.map(row => `<li>Lock ID: ${row.lock_id}, Owner: ${row.owner}</li>`).join('')}
+            </ul>
+        `;
+
+        // Append the section to the results container
+        resultsContainer.appendChild(tierSection);
+    }
+}
+
+// Main function to fetch data and render tiers and totals when the page loads
+async function main() {
+    try {
+        const data = await fetchData();
+        const tiersAndTotals = calculateTiersAndTotals(data);
+        renderTiersAndTotals(tiersAndTotals);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+// Call the main function when the page loads
+window.addEventListener('load', main);
